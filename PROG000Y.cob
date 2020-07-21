ef41cfdc2c088e66a8b13d1c84d5ad3c13e69577
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-07-21 16:56:55 -0300

    Regras de um programa COBOL

---
+++ PROG000Y.cob
@@ -0,0 +1,18 @@
+      ******************************************************************
+      * Author: Eduardo Freitas
+      * Date: 21/07/2020
+      * Purpose: Regras de um programa COBOL
+      * Tectonics: cobc
+      ******************************************************************
+         1         2         3         4         5         6         7         8
+12345678901234567890123456789012345678901234567890123456789012345678901234567890
+      *Comentário
+      -Continuação
+
+      *A..A
+      *Onde ficam divisões de sessões, parágrafos e alguns níveis de variáveis
+
+      *    B...........................................................B
+      *Onde estão os parâmetros, instruções e comandos COBOL
+
+      *73 a 80: não utilizada na programação COBOL
